cc91fb46213ce992869f0df6425901af4c70bbaa
Gokul2406 <gokulpbharathan89@gmail.com>
2021-08-07 15:51:13 +0530

    Now the programme can find files with hardcoded extensions

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,20 @@
 pub mod run {
     pub fn run() {
-       let files = std::fs::read_dir(".").unwrap();
-       for file in files {
-           if let Ok(f) = file {
-               if let Some(f_as_str) = f.file_name().to_str() {
-                println!("{}", f_as_str);
-               }
-               }
-           }
-       }
+        let files = std::fs::read_dir(".").unwrap();
+        let required_ext = "toml";
+        for file in files {
+            if let Ok(f) = &file {
+                if let Some(f_as_str) = f.file_name().to_str() {
+                    let file_ext: Vec<&str> = f_as_str.split(".").collect();
+                    for ext in &file_ext {
+                        if ext.contains(required_ext) {
+                            for matched_file in &file {
+                                println!("{:?}", matched_file.file_name()); 
+                            }
+                        }
+                    }
+                }
+            }
+        }
     }
+}

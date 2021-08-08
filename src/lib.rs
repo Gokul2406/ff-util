bf24e7ed8cbd3590f8cf9e6c7ec42088b9a7c501
Gokul2406 <gokulpbharathan89@gmail.com>
2021-08-08 15:13:18 +0530

    Change version to 0.1.1 and add a bit of docs

--- src/lib.rs
+++ src/lib.rs
@@ -3,7 +3,12 @@ pub mod run {
 
     use colored::*;
     pub fn run(dir: &Path, required_ext: &String) {
+        // We use the read_dir method to read the directory given by the user and it is stored in a
+        // variable for future use
         let files = std::fs::read_dir(dir).unwrap();
+
+        // Now we loop over the files vector and checks if the file contains the extension or the
+        // filename provided by the user
         for file in files {
             if let Ok(f) = &file {
                 if let Some(f_as_str) = f.file_name().to_str() {
